c6beddaca2cc80072e6d0de1d043369fc008c46c
David <davidmollera@hotmail.com>
2022-11-01 21:59:01 -0500

    created a function to render all the books, this will grab the book info from a set of data and make them work dependently on the set of data updating automatically

--- books.js
+++ books.js
@@ -1,4 +1,35 @@
+function renderBooks(){
+    const booksWrapper = document.querySelector('.books')
+    const books = getBooks()
+    const book = books.map((book) => {
+       return `<div class="book">
+        <figure class="book__img--wrapper">
+          <img class="book__img" src="${book.url}" alt=""/>
+        </figure>
+        <div class="book__title">
+          ${book.title}
+        </div>
+        <div class="book__ratings">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star-half-alt"></i>
+        </div>
+        <div class="book__price">
+          <span class="book__price--normal">$${book.originalPrice.toFixed(2)}</span>
+        </div>
+      </div>`
+    }).join('')
 
+    booksWrapper.innerHTML = book
+console.log(book)
+}
+
+
+setTimeout(() => {
+    renderBooks()
+})
 
 // FAKE DATA
 function getBooks() {
@@ -6,7 +37,7 @@ function getBooks() {
       {
         id: 1,
         title: "Crack the Coding Interview",
-                  url: "assets/crack the coding interview.png",
+        url: "assets/crack the coding interview.png",
         originalPrice: 49.95,
         salePrice: 14.95,
         rating: 4.5,
